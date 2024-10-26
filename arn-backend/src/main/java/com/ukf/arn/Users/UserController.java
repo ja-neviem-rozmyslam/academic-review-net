15346238565d4d296fb824b186156d7d34598b55
Patrik <horvathpatrik0@gmail.com>
2024-10-26 18:52:16 +0200

    [ARN-49] - User login api

---
+++ arn-backend/src/main/java/com/ukf/arn/Users/UserController.java
@@ -0,0 +1,28 @@
+package com.ukf.arn.Users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestParam String email,
+            @RequestParam String password) {
+
+        return userService.login(email, password);
+    }
+}
