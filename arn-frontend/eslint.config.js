417704282ddf3d6f027c328aaaabf19447bf999b
Richard <rici.bernath@gmail.com>
2024-10-26 15:48:16 +0200

    [ARN-49] - Lint fix

--- arn-frontend/eslint.config.js
+++ arn-frontend/eslint.config.js
@@ -14,6 +14,7 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      "@typescript-eslint/no-inferrable-types": "off",
       "@angular-eslint/directive-selector": [
         "error",
         {
@@ -38,6 +39,9 @@ module.exports = tseslint.config(
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/click-events-have-key-events": "off",
+      "@angular-eslint/template/interactive-supports-focus": "off"
+    }
   }
 );
