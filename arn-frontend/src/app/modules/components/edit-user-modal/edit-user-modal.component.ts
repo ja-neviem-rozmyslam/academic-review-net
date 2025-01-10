11f2d2d5b6e9bc6ea10169240f2331fab569db23
Lukas Janek <lukas.janek1111@gmail.com>
2025-01-10 23:10:52 +0100

    [ARN-43] - Edit User component

---
+++ arn-frontend/src/app/modules/components/edit-user-modal/edit-user-modal.component.ts
@@ -0,0 +1,55 @@
+import {Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
+import {BaseModal} from '../base-modal/entities/BaseModal';
+import {UserDetails} from '../../main-panel/profile-page/profile-settings/entities/UserDetails';
+import {EmailDomain} from '../registration-panel/entities/EmailDomain';
+import {ProfileSettingsService} from '../../main-panel/profile-page/profile-settings/service/profile-settings.service';
+import {EmailDomainService} from '../registration-panel/services/email-domain.service';
+
+@Component({
+  selector: 'app-edit-user-modal',
+  templateUrl: './edit-user-modal.component.html',
+  styleUrl: './edit-user-modal.component.less'
+})
+export class EditUserModalComponent extends BaseModal implements OnInit{
+
+  @Output() profileUpdated = new EventEmitter<void>();
+
+  userDetails: UserDetails;
+  emailDomains: EmailDomain[];
+  universitiesSelectOptions: any[];
+  isFetching = true;
+
+  constructor(@Inject('modalData') public data: any, private profileSettingsService: ProfileSettingsService, private emailDomainService: EmailDomainService) {
+    super();
+
+    this.userDetails = {
+      email: data.user.email,
+      name: data.user.name,
+      surname: data.user.surname,
+      universityId: data.user.university.id,
+      role: data.user.role
+    };
+  }
+
+  ngOnInit() {
+    this.emailDomainService.getAllDomains().pipe().subscribe((domains: EmailDomain[]) => {
+      this.emailDomains = domains;
+      this.universitiesSelectOptions = this.emailDomains.map(university => ({
+        value: university.universityId,
+        display: university.universityName,
+        selectObject: university
+      }));
+      this.isFetching = false;
+    });
+  }
+
+  updateProfile(): void {
+    //console.log(this.userDetails);
+    this.isFetching = true;
+    this.profileSettingsService.updateProfile(this.userDetails).pipe().subscribe(() => {
+      this.profileUpdated.emit();
+      this.closeModal();
+    });
+  }
+
+}
