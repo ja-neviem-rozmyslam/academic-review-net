4680a87f822de7df7724c4a1c13640369bf75588
Patrik <horvathpatrik0@gmail.com>
2025-01-21 12:50:49 +0100

    doker

--- config/mysql/db.sql
+++ config/mysql/db.sql
@@ -4,187 +4,136 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `arn_noddedoil` DEFAULT CHARACTER SET utf8mb4 ;
+USE `arn_noddedoil` ;
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Table `arn_noddedoil`.`universities`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `defaultdb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema defaultdb
--- -----------------------------------------------------
-USE `defaultdb` ;
-
--- -----------------------------------------------------
--- Table `defaultdb`.`roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`roles` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `role` VARCHAR(45) NOT NULL,
-  `roleIdent` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`universities` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `university_name` VARCHAR(512) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`universities`
+-- Table `arn_noddedoil`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`universities` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `university_name` VARCHAR(512) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`users` (
+    `id` VARCHAR(36) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `surname` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `registration_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `university_id` BIGINT NULL,
+    `verified` TINYINT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_users_universities1_idx` (`university_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`faculties`
+-- Table `arn_noddedoil`.`conferences`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`faculties` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `faculty_name` VARCHAR(512) NOT NULL,
-  `university_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_faculties_universities_idx` (`university_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`conferences` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `conference_name` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(45) NULL,
+    `upload_deadline` DATETIME NOT NULL,
+    `review_deadline` DATETIME NOT NULL,
+    `creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `faculty` VARCHAR(45) NOT NULL,
+    `review_form` JSON NULL,
+    `closed` TINYINT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`users`
+-- Table `arn_noddedoil`.`thesis_types`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`users` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `surname` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `registration_date` TIMESTAMP NOT NULL,
-  `faculties_id` BIGINT NULL,
-  `universities_id` BIGINT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_users_faculties1_idx` (`faculties_id` ASC) VISIBLE,
-  INDEX `fk_users_universities1_idx` (`universities_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`thesis_types` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`theses_states`
+-- Table `arn_noddedoil`.`uploaded_theses`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`theses_states` (
-  `stateIdent` VARCHAR(2) NOT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`stateIdent`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`uploaded_theses` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `thesis_title` VARCHAR(255) NOT NULL,
+    `type_id` BIGINT NOT NULL,
+    `abstract_sk` LONGTEXT NOT NULL,
+    `abstract_en` LONGTEXT NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `conference_id` BIGINT NOT NULL,
+    `author_id` VARCHAR(36) NOT NULL,
+    `coauthors` VARCHAR(512) NULL,
+    `reviewer_id` VARCHAR(36) NULL,
+    `review` JSON NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_uploaded_theses_conferences1_idx` (`conference_id` ASC) VISIBLE,
+    INDEX `fk_uploaded_theses_users1_idx` (`reviewer_id` ASC) VISIBLE,
+    INDEX `fk_uploaded_theses_theses_categories1_idx` (`type_id` ASC) VISIBLE,
+    INDEX `fk_uploaded_theses_users2_idx` (`author_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`conference_states`
+-- Table `arn_noddedoil`.`email_domains`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`conference_states` (
-  `stateIdent` VARCHAR(2) NOT NULL,
-  `state` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`stateIdent`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`email_domains` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `email_domain` VARCHAR(45) NOT NULL,
+    `university_id` BIGINT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_email_domains_universities1_idx` (`university_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`conferences`
+-- Table `arn_noddedoil`.`users_in_conferences`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`conferences` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `conference_name` VARCHAR(255) NOT NULL,
-  `upload_deadline` DATETIME NOT NULL,
-  `review_deadline` DATETIME NOT NULL,
-  `creation_date` TIMESTAMP NOT NULL,
-  `conference_state` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_conferences_conference_states1_idx` (`conference_state` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`users_in_conferences` (
+    `user_id` VARCHAR(36) NOT NULL,
+    `conference_id` BIGINT NOT NULL,
+    PRIMARY KEY (`user_id`, `conference_id`),
+    INDEX `fk_conferences_has_users_users1_idx` (`user_id` ASC) VISIBLE,
+    INDEX `fk_conferences_has_users_conferences1_idx` (`conference_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`uploaded_theses`
+-- Table `arn_noddedoil`.`roles`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`uploaded_theses` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `thesis_title` VARCHAR(255) NOT NULL,
-  `abstract_sk` LONGTEXT NOT NULL,
-  `abstract_en` LONGTEXT NOT NULL,
-  `folder_hash` TINYTEXT NOT NULL,
-  `timestamp` TIMESTAMP NOT NULL,
-  `student_id` BIGINT NOT NULL,
-  `reviewer_id` BIGINT NULL,
-  `conferences_id` BIGINT NOT NULL,
-  `these_state` VARCHAR(2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_uploaded_theses_theses_states1_idx` (`these_state` ASC) VISIBLE,
-  INDEX `fk_uploaded_theses_users1_idx` (`reviewer_id` ASC) VISIBLE,
-  INDEX `fk_uploaded_theses_users2_idx` (`student_id` ASC) VISIBLE,
-  INDEX `fk_uploaded_theses_conferences1_idx` (`conferences_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`roles` (
+    `user_id` VARCHAR(36) NOT NULL,
+    `role_ident` VARCHAR(2) NOT NULL,
+    PRIMARY KEY (`user_id`, `role_ident`),
+    INDEX `fk_users_has_roles_users1_idx` (`user_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `defaultdb`.`email_domains`
+-- Table `arn_noddedoil`.`tokens`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`email_domains` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `email_domain` VARCHAR(45) NOT NULL,
-  `universities_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_email_domains_universities1_idx` (`universities_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `arn_noddedoil`.`tokens` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `token` VARCHAR(255) NOT NULL,
+    `user_id` VARCHAR(36) NOT NULL,
+    `expiration_time` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `token_type` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_password_reset_tokens_users1_idx` (`user_id` ASC) VISIBLE)
+    ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `defaultdb`.`rating_categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`rating_categories` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `question` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `defaultdb`.`user_roles`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`user_roles` (
-  `role_id` BIGINT NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  PRIMARY KEY (`role_id`, `user_id`),
-  INDEX `fk_roles_has_users_users1_idx` (`user_id` ASC) VISIBLE,
-  INDEX `fk_roles_has_users_roles1_idx` (`role_id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `defaultdb`.`users_in_conferences`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`users_in_conferences` (
-  `conferences_id` BIGINT NOT NULL,
-  `users_id` BIGINT NOT NULL,
-  PRIMARY KEY (`conferences_id`, `users_id`),
-  INDEX `fk_conferences_has_users_users1_idx` (`users_id` ASC) VISIBLE,
-  INDEX `fk_conferences_has_users_conferences1_idx` (`conferences_id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `defaultdb`.`theses_ratings`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `defaultdb`.`theses_ratings` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `category_id` BIGINT NOT NULL,
-  `user_id` BIGINT NOT NULL,
-  `rating` INT NOT NULL,
-  `comment` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_users_has_rating_categories_rating_categories1_idx` (`category_id` ASC) VISIBLE,
-  INDEX `fk_users_has_rating_categories_users1_idx` (`id` ASC) VISIBLE,
-  INDEX `fk_theses_ratings_uploaded_theses1_idx` (`user_id` ASC) VISIBLE)
-ENGINE = InnoDB;
-
+USE `arn_noddedoil` ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- config/mysql/inserts.sql
+++ config/mysql/inserts.sql
@@ -1,21 +1,17 @@
-INSERT INTO defaultdb.users (id, name, surname, email, password, verified)
+INSERT INTO arn_noddedoil.users (id, name, surname, email, password, verified)
 SELECT 'b1d74c48-80e9-429f-a463-9ccb6b777db3', 'Super', 'Admin', 'admin',
        '$2a$10$g09D8F7ABajnAN00MxOScuDDB1Rbe56gTHl4gLUiYDxxndGHQ8kH6', true
 WHERE NOT EXISTS (
     SELECT 1
-    FROM defaultdb.users
+    FROM arn_noddedoil.users
     WHERE id = 'b1d74c48-80e9-429f-a463-9ccb6b777db3'
 );
 
-INSERT INTO defaultdb.roles (user_id, role_ident)
+INSERT INTO arn_noddedoil.roles (user_id, role_ident)
 SELECT 'b1d74c48-80e9-429f-a463-9ccb6b777db3', 'SA'
 WHERE NOT EXISTS (
     SELECT 1
-    FROM defaultdb.roles
+    FROM arn_noddedoil.roles
     WHERE user_id = 'b1d74c48-80e9-429f-a463-9ccb6b777db3'
       AND role_ident = 'SA'
 );
-
-CREATE USER 'root'@'%' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
